8398229b6cb222846251374dc195d7e06c0eae05
RaelZero <rael@oddworlds.org>
2020-12-03 10:14:42 +0100

    Made a skeleton file not to get crazy every time

---
+++ skeleton.py
@@ -0,0 +1,23 @@
+def parseInput():
+    input = []
+
+    with open('input.txt') as inputFile:
+        for line in inputFile:
+            input.append(line)
+
+    return input
+
+def partOne(input):
+    pass
+
+def partTwo(input):
+    pass
+
+def main():
+    input = parseInput()
+
+    partOne(input)
+    partTwo(input)
+
+if __name__ == "__main__":
+    main()
