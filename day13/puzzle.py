6af5dcf4da8443d639f490a3a8f103f9c246f70d
RaelZero <gaz.gong@gmail.com>
2020-12-13 15:40:49 +0100

    Part one done! ezpz

--- day13/puzzle.py
+++ day13/puzzle.py
@@ -2,16 +2,32 @@ def parseInput():
     with open('input.txt') as inputFile:
         startTime = int(inputFile.readline())
         buses = []
+        schedule = []
+
         for b in str.split(inputFile.readline()[:-1], ','):
             if b != 'x':
-                buses.append(b)
+                schedule.append(int(b))
+                buses.append(int(b))
+            else:
+                schedule.append(b)
 
-    input = (startTime, buses)
+    input = (startTime, buses, schedule)
 
     return input
 
 def partOne(input):
-    pass
+    arrivalTime = input[0]
+    buses = input[1]
+    nextDepartures = {bus: (arrivalTime//bus + 1) * bus for bus in buses}
+
+    firstBus, nextDeparture = sorted(nextDepartures.items(), key = lambda i: i[1])[0]
+
+    delta = nextDeparture - arrivalTime
+
+    result = firstBus * delta
+
+    print("Part one: " + str(result))
+    return(result)
 
 def partTwo(input):
     pass
